5d931e45e69511d5a3a511933cc3c6ca459360b8
muhfizh <muhfizhufi@gmail.com>
2025-01-10 10:14:18 +0700

    menambahkan kirim file

--- public/chat.js
+++ public/chat.js
@@ -7,6 +7,7 @@ window.onload = function() {
     var content = document.getElementById("content");
     var name = document.getElementById("name");
     var color = document.getElementById("favcolor");
+    var picture = document.getElementById("file");
 
     socket.on('message', function (data) {
         if(data.message) {
@@ -15,7 +16,13 @@ window.onload = function() {
             var html = '';
             for(var i=0; i<message.length; i++){
                 html += '<b style="color:'+ (message[i].color) +'">' + (message[i].username ? message[i].username : 'Server') + ': </b>';
-                html += message[i].message + '<br />';
+                if (message[i].message && message[i].picture){
+                    html += message[i].message + '<img src="' + message[i].picture + ';" style="width:auto;"> <br />';
+                } else if (message[i].message) {
+                    html += message[i].message + '<br />';
+                } else if (message[i].picture) {
+                    html += '<img src="' + message[i].picture + ';" style="width:auto;"> <br />';
+                }
             }
             content.innerHTML = html;
             content.scrollTop = content.scrollHeight;
@@ -29,7 +36,7 @@ window.onload = function() {
             alert("Tolong isi nama anda!");
         } else {
             var text = field.value;
-            socket.emit('send', {message: text, username: name.value, color: color.value});
+            socket.emit('send', {message: text, username: name.value, color: color.value, picture: picture.value});
             field.value = '';
         }
     };
